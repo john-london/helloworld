2f1e4a4da872fdd89ac836464a217d3a76553ea2
john-london <blackkite@server.blackkitetechnology.com>
2014-03-14 16:35:10 +0000

    Initial Commit

---
+++ helloworld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  helloworld
+//
+//  Created by Black Kite Admin on 14/03/2014.
+//  Copyright (c) 2014 Black Kite Technology. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
