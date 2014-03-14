d6e09be21164c6832708d1d2bbbdb09dae8b3f55
john-london <blackkite@server.blackkitetechnology.com>
2014-03-14 16:38:43 +0000

    2nd commit

--- helloworld/main.c
+++ helloworld/main.c
@@ -12,7 +12,7 @@ int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Hello, World!\nThat was a painful Github setup.\n");
     return 0;
 }
 
